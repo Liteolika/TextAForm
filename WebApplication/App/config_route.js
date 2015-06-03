fd30fb65337892866c3556b90f48e8e84f6bfea9
Peter Carlsson <peter@liteolika.se>
2015-06-03 20:11:27 +0200

    Byggt om lite saker.

--- WebApplication/App/config_route.js
+++ WebApplication/App/config_route.js
@@ -6,50 +6,66 @@
         $urlRouterProvider.otherwise("/home");
 
         $stateProvider
-        .state('home', {
-            url: "/home",
-            templateUrl: "/app/home/home.html"
-        })
-        .state('about', {
-            url: "/about",
-            templateUrl: "/app/about/about.html"
-        })
-        .state('contact', {
-            url: "/contact",
-            templateUrl: "/app/contact/contact.html"
-        })
-        .state('customers', {
-            url: "/customers",
-            templateUrl: "/app/customers/customers.html"
-        })
-        .state('customers-spsf', {
-            url: "/customers-spsf",
-            templateUrl: "/app/customers/spsf/spsf.html"
-        })
-            .state('customers-lobb', {
-                url: "/customers-lobb",
-                templateUrl: "/app/customers/lobb/lobb.html"
+
+            .state('home', {
+                url: "/home",
+                templateUrl: "/app/home/home.html"
             })
+
+            .state('about', {
+                url: "/about",
+                templateUrl: "/app/about/about.html"
+            })
+
+            .state('contact', {
+                url: "/contact",
+                templateUrl: "/app/contact/contact.html"
+            })
+
+            .state('customers', {
+                url: "/customers",
+                templateUrl: "/app/customers/customers.html"
+            })
+
+            .state('customers-spsf', {
+                url: "/customers-spsf",
+                templateUrl: "/app/customers/spsf/spsf.html"
+            })
+
+            .state('customers-lob', {
+                url: "/customers-lob",
+                templateUrl: "/app/customers/lob/lob.html"
+            })
+
             .state('customers-845', {
                 url: "/customers-845",
                 templateUrl: "/app/customers/845/845.html"
             })
+
             .state('customers-books', {
                 url: "/customers-books",
                 templateUrl: "/app/customers/books/books.html"
             })
+
             .state('customers-hh', {
                 url: "/customers-hh",
                 templateUrl: "/app/customers/hh/hh.html"
             })
+
             .state('customers-softit', {
                 url: "/customers-softit",
                 templateUrl: "/app/customers/softit/softit.html"
             })
-        .state('references', {
-            url: "/references",
-            templateUrl: "/app/references/references.html"
-        });
+
+            //.state('uffepuffe', {                  <--- Unikt namn på state
+            //    url: "/uffessida",                 <--- Vad som ska visas i webbläsarens adressrad
+            //    templateUrl: "/app/uffe/uffe.html" <--- Sökvägen till sidan
+            //})
+
+            .state('references', {
+                url: "/references",
+                templateUrl: "/app/references/references.html"
+            });
 
     });
 
